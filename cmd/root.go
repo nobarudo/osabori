1611ba4ba719b5dc6641666a0917bb7be333f953
nobarudo <esukab@gmail.com>
2025-12-17 03:22:17 +0900

    feat:5分間無意味なダウンロードバーを表示し続ける機能の追加

--- cmd/root.go
+++ cmd/root.go
@@ -1,27 +1,21 @@
 /*
 Copyright © 2025 NAME HERE <EMAIL ADDRESS>
-
 */
 package cmd
 
 import (
-	"os"
+	"log/slog"
+	"time"
 
+	"github.com/schollz/progressbar/v3"
 	"github.com/spf13/cobra"
 )
-
-
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "osabori",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "This command will output a prompt like you are working",
+	Long: `This command will output a prompt like you are working`,
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
 	// Run: func(cmd *cobra.Command, args []string) { },
@@ -29,11 +23,24 @@ to quickly create a Cobra application.`,
 
 // Execute adds all child commands to the root command and sets flags appropriately.
 // This is called by main.main(). It only needs to happen once to the rootCmd.
-func Execute() {
-	err := rootCmd.Execute()
-	if err != nil {
-		os.Exit(1)
+func OutputLog() {
+	slog.Info("fake Download. Don't worry,　not downloading anything.")
+	for i := 1; i <= 5; i++ {
+		for j := 0; j < 6; j++ {
+			fakeDownloadBar()
+		}
+		slog.Warn("one minute has passed")	
 	}
+	
+}
+
+func fakeDownloadBar() {
+	bar := progressbar.Default(100,"downloading")
+	for i := 0; i < 100; i++ {
+		time.Sleep(100 * time.Millisecond)
+    	bar.Add(1)	
+	}
+	bar.Finish()
 }
 
 func init() {
--- main.go
+++ main.go
@@ -1,11 +1,10 @@
 /*
 Copyright © 2025 NAME HERE <EMAIL ADDRESS>
-
 */
 package main
 
 import "osabori/cmd"
 
 func main() {
-	cmd.Execute()
+	cmd.OutputLog()
 }
